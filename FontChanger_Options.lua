de1f2469f7a5dc35c94a6f7139837178fd2767bd
Michael <zetauma@fastmail.com>
2024-04-16 17:34:56 -0400

    added batch script to generate slugs
    - generates slugs for all fonts in fonts/ttfs folder
    - auto-updates FontChanger_Options.lua with new fonts

---
+++ FontChanger_Options.lua
@@ -0,0 +1,17 @@
+-- DON'T EDIT THIS FILE DIRECTLY -- 
+-- technically you can, but you should really place your 
+-- font files in the fonts/ttfs directory and re-run fonts/slug.bat 
+FontChanger = {} 
+local FC = FontChanger or {} 
+FC.FONT_CHOICES = { 
+	"FCBOOK", 
+	"FCChat", 
+	"FCUI", 
+	"FCUI_Bold", 
+} 
+FC.FONT_VALUES = { 
+	"FontChanger/fonts/slugs/FCBOOK.slug", 
+	"FontChanger/fonts/slugs/FCChat.slug", 
+	"FontChanger/fonts/slugs/FCUI.slug", 
+	"FontChanger/fonts/slugs/FCUI_Bold.slug", 
+} 
