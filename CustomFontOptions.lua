e715978d031fad04095e991a7dea9aff05c2e37b
Michael <m.king@fastmail.com>
2026-04-06 10:14:43 -0400

    Update CustomFontOptions.lua

--- CustomFontOptions.lua
+++ CustomFontOptions.lua
@@ -1,16 +1,17 @@
--- DON'T EDIT THIS FILE DIRECTLY -- 
--- technically you can, but you should really place your 
--- font files in the fonts/ttfs directory and re-run fonts/slug.bat 
-local FC = FontChanger or {} 
-FC.CUSTOM_FONT_CHOICES = { 
-	"FCBook", 
-	"FCChat", 
-	"FCUI", 
-	"FCUI_Bold", 
-} 
-FC.CUSTOM_FONT_VALUES = { 
-	"FontChanger/fonts/slugs/FCBook.slug", 
-	"FontChanger/fonts/slugs/FCChat.slug", 
-	"FontChanger/fonts/slugs/FCUI.slug", 
-	"FontChanger/fonts/slugs/FCUI_Bold.slug", 
-} 
+-- DON'T EDIT THIS FILE DIRECTLY --
+-- technically you can, but you should really place your
+-- font files in the fonts/ttfs directory and re-run fonts/slug.bat (Windows)
+-- or fonts/slug.sh (Linux)
+local FC = FontChanger or {}
+FC.CUSTOM_FONT_CHOICES = {
+    "FCBook",
+    "FCChat",
+    "FCUI_Bold",
+    "FCUI",
+}
+FC.CUSTOM_FONT_VALUES = {
+    "FontChanger/fonts/slugs/FCBook.slug",
+    "FontChanger/fonts/slugs/FCChat.slug",
+    "FontChanger/fonts/slugs/FCUI_Bold.slug",
+    "FontChanger/fonts/slugs/FCUI.slug",
+}
